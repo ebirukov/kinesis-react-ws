7b3994290140af5986f3297cd30e460ea5de154d
e.birukov <e.birukov@corpwebgames.com>
2020-02-19 19:38:32 +0300

    window by time and num of requests

--- src/main/java/org/johnsoft/reactive/KinesisService.java
+++ src/main/java/org/johnsoft/reactive/KinesisService.java
@@ -1,9 +1,14 @@
 package org.johnsoft.reactive;
 
+import io.netty.buffer.ByteBuf;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import software.amazon.awssdk.core.SdkBytes;
+import software.amazon.awssdk.core.exception.SdkClientException;
+import software.amazon.awssdk.core.exception.SdkException;
+import software.amazon.awssdk.core.exception.SdkInterruptedException;
 import software.amazon.awssdk.http.Protocol;
 import software.amazon.awssdk.http.nio.netty.NettyNioAsyncHttpClient;
 import software.amazon.awssdk.services.kinesis.KinesisAsyncClient;
@@ -13,7 +18,10 @@ import java.net.URI;
 import java.nio.ByteBuffer;
 import java.time.Duration;
 import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.*;
 
@@ -24,12 +32,12 @@ public class KinesisService {
     public static final String DEFAULT_KINESIS_ENDPOINT = "https://kinesis.us-east-1.amazonaws.com";
     private final KinesisAsyncClient kinesisClient;
     private String streamName;
+    AtomicLong counter = new AtomicLong();
 
     private static final Logger log = LoggerFactory.getLogger(KinesisService.class);
 
     public KinesisService() {
         System.setProperty("aws.cborEnabled", "false");
-        //System.setProperty("aws.cborEnabled", "false");
         Properties config = ApplicationProperties.loadProperties(CONFIG_FILE);
 
         String endpoint = config.getProperty("kinesis.endpoint", DEFAULT_KINESIS_ENDPOINT);
@@ -52,27 +60,30 @@ public class KinesisService {
                     log.info(streamName + " stream was created");
                     return response;
                 })
-/*                .exceptionally(throwable -> {
-                    if (throwable instanceof KinesisException) {
-                        log.error("can't create stream " + streamName, throwable);
-                    } else {
-                        log.error("unkhown error while creating stream " + streamName, throwable);
-                    }
-                    return null;
-                })*/
         ).onErrorResume(ResourceInUseException.class, e -> Mono.empty())
         .block(Duration.ofMillis(1000));
     }
 
+    public Mono<ByteBuffer> send(Flux<ByteBuffer> messages) {
+        return send(messages.toStream());
+    }
+
     public Mono<ByteBuffer> send(List<ByteBuffer> messages) {
+        return send(messages.stream());
+    }
+
+    private Mono<ByteBuffer> send(Stream messageStream) {
         return Mono.fromFuture(
-                kinesisClient.putRecords(createPutRecordsRequest(messages))
+                kinesisClient.putRecords(createPutRecordsRequest(messageStream))
                         .thenApply(this::processResult)
         ).onErrorStop()
+                .onErrorReturn(SdkClientException.class, ByteBuffer.wrap(("TRY AGAIN").getBytes()))
+                //.onErrorReturn(SdkInterruptedException.class, ByteBuffer.wrap(("Interrupted").getBytes()))
                 .onErrorReturn(KinesisException.class, ByteBuffer.wrap(("FAILURE").getBytes()));
     }
 
     private ByteBuffer processResult(PutRecordsResponse response) {
+        if (response.failedRecordCount() == 0) return ByteBuffer.wrap(String.valueOf(response.records().size()).getBytes());
         return ByteBuffer.wrap(response.records()
                 .stream()
                 .filter(r -> r.errorCode() != null)
@@ -86,7 +97,6 @@ public class KinesisService {
     }
 
     private String formatErrorResult(Map<String, Long> numOfErrorByType) {
-        System.out.println(numOfErrorByType);
         StringJoiner joiner = new StringJoiner(",");
         numOfErrorByType.forEach((type, num) -> joiner.add(String.join("-", type, num.toString())));
         return joiner.toString();
@@ -103,15 +113,15 @@ public class KinesisService {
         return ByteBuffer.wrap(r.sequenceNumber().getBytes());
     }
 
-    private PutRecordsRequest createPutRecordsRequest(List<ByteBuffer> messages) {
+    private PutRecordsRequest createPutRecordsRequest(Stream<ByteBuffer> messageStream) {
         return PutRecordsRequest.builder()
                 .streamName(streamName)
-                .records(createPutRecordsRequestsBuilder(messages))
+                .records(createPutRecordsRequestsBuilder(messageStream))
                 .build();
     }
 
-    private Collection<PutRecordsRequestEntry> createPutRecordsRequestsBuilder(List<ByteBuffer> messages) {
-        return messages.stream()
+    private Collection<PutRecordsRequestEntry> createPutRecordsRequestsBuilder(Stream<ByteBuffer> messageStream) {
+        return messageStream
                 .map(m -> PutRecordsRequestEntry.builder()
                         .partitionKey(UUID.randomUUID().toString())
                         .data(SdkBytes.fromByteBuffer(m))
--- src/main/java/org/johnsoft/reactive/WebSocketCommunicationHandler.java
+++ src/main/java/org/johnsoft/reactive/WebSocketCommunicationHandler.java
@@ -3,35 +3,34 @@ package org.johnsoft.reactive;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import org.reactivestreams.Publisher;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
 import reactor.netty.http.websocket.WebsocketInbound;
 import reactor.netty.http.websocket.WebsocketOutbound;
 
 import java.nio.ByteBuffer;
 import java.time.Duration;
-import java.time.temporal.ChronoUnit;
 import java.util.List;
-import java.util.UUID;
-import java.util.stream.Collectors;
 
-import static java.util.stream.Collectors.*;
+import static java.util.stream.Collectors.toList;
 
 public class WebSocketCommunicationHandler {
 
     private final KinesisService kinesisService = new KinesisService();
 
     Publisher<Void> process(WebsocketInbound in, WebsocketOutbound out) {
-        return out.send(in.receive()
-                        .retain()
-                        .buffer(500)
-                        //.buffer(Duration.ofMillis(1))
-                        .map(this::convert)
-                        .map(kinesisService::send)
-                        .flatMap(r -> r.map(Unpooled::wrappedBuffer))
-                        .log()
-                         );
+        return in.receive()
+                .retain()
+                .window(Duration.ofMillis(10))
+                .map(w -> w.buffer(500)
+                                .map(this::convert)
+                                .map(kinesisService::send)
+                                .onBackpressureBuffer(500)
+                                //.log("", Level.INFO, false, SignalType.ON_ERROR)
+                                .flatMap(r -> r.map(Unpooled::wrappedBuffer))
+                                .publish(out::send)
+                ).doOnCancel(() -> System.out.println("cancel"))
+                .doOnError(e -> e.printStackTrace())
+                .flatMap(Flux::from);
     }
 
     private List<ByteBuffer> convert(List<ByteBuf> byteBuf) {
